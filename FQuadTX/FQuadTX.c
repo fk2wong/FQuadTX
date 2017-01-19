0036313b4dddbe70bf735b78118ec050f606499f
Felix Wong <Felix Wong>
2017-01-19 12:23:57 -0500

    WIP, does not build.

--- FQuadTX/FQuadTX.c
+++ FQuadTX/FQuadTX.c
@@ -19,10 +19,7 @@
 int main( void )
 {
 	FStatus status;
-	bool isButtonPressed; // TEMP
-	uint64_t oldTime;
-	uint64_t newTime;
-	bool shouldResetOnNextPress = true;
+	PlatformTimer_Init();
 	
 	// Initialize power
 	status = FQuadTXPower_Init();
@@ -32,68 +29,17 @@ int main( void )
 	status = FQuadTXPower_Hold();
 	require_noerr( status, exit );
 	
-	// Initialize LED indicator
-	status = FQuadTXLED_Init();
-	require_noerr( status, exit );
-	
-	// Turn on the LED to show the system is on
-	status = FQuadTXLED_On();
-	require_noerr( status, exit );
-	
 	// Initialize buttons, joysticks and triggers
 	status = FQuadTXPad_Init();
 	require_noerr( status, exit );
 	
-	// TEMP
-	status = PlatformTimer_Init();
-	require_noerr( status, exit );
-	
-	status = PlatformTimer_Reset();
-	require_noerr( status, exit );
-	
-	status = PlatformTimer_GetTime( &oldTime );
-	require_noerr( status, exit );
-	
 	while(1)
 	{
-		status = PlatformTimer_GetTime( &newTime );
-		require_noerr( status, exit );
 		
-
+		status = FQuadTXPower_CheckPowerOffRequest();	
+		require_noerr( status, exit );
 		
 		// 		_delay_ms( 200 );
-		//
-		//
-		// TEMP turning off power
-		status = FQuadTXPad_ReadButtonState( FQuadTXPadButton_Center, &isButtonPressed );
-		require_noerr( status, exit );
-		
-		if ( !isButtonPressed )
-		{
-			PlatformTimer_Reset();
-			shouldResetOnNextPress = true;
-		}
-		else
-		{
-			if ( shouldResetOnNextPress )
-			{
-				status = PlatformTimer_GetTime( &oldTime );
-				require_noerr( status, exit );
-				shouldResetOnNextPress = false;
-			}
-			
-			status = PlatformTimer_GetTime( &newTime );
-			require_noerr( status, exit );
-			
-			if (( newTime - oldTime ) > 2000 )
-			{
-				status = FQuadTXPower_Release();
-				require_noerr( status, exit );
-				
-				status = FQuadTXLED_Off();
-				require_noerr( status, exit );
-			}
-		}
 	}
 	
 exit:
--- FQuadTX/FQuadTX_Libs/FQuadTXPower/FQuadTXPower.c
+++ FQuadTX/FQuadTX_Libs/FQuadTXPower/FQuadTXPower.c
@@ -6,8 +6,16 @@
  */ 
 
 #include "FQuadTXPower.h"
+#include "FQuadTXLED.h"
+#include "FQuadTXPad.h"
 #include "FQuadTXLogging.h"
 #include "Platform_FQuadTX.h"
+#include <stdbool.h>
+
+#define FQUADTX_POWER_RELEASE_REQUEST_WAIT_MS ( 2000 )
+
+static uint64_t mLastPressedTime;
+static uint64_t mShouldResetTimerOnNextPress;
 
 FStatus FQuadTXPower_Init()
 {
@@ -22,6 +30,22 @@ FStatus FQuadTXPower_Init()
 	platformStatus = PlatformGPIO_Configure( FQuadTXGPIO_PowerHold, PlatformGPIOConfig_Output );
 	require_noerr( platformStatus, exit );
 	
+	// Configure the LED that indicates the power is on
+	status = FQuadTXLED_Init();
+	require_noerr( status, exit );
+	
+	// Init the pad buttons, since the center button is used to request a power off
+	// Note the pad may already be init by other modules
+	status = FQuadTXPad_Init();
+	require(( status == FStatus_Success ) || ( status = FStatus_AlreadyInitialized ) , exit );
+	
+	// Initialize the timer, which is used for the soft power off
+// 	platformStatus = PlatformTimer_Init();
+// 	require_noerr( platformStatus, exit );
+// 		
+// 	platformStatus = PlatformTimer_Reset();
+// 	require_noerr( platformStatus, exit );
+	
 	status = FStatus_Success;
 exit:
 	return status;
@@ -35,6 +59,10 @@ FStatus FQuadTXPower_Hold()
 	// Hold power on in order to stay alive
 	platformStatus = PlatformGPIO_OutputHigh( FQuadTXGPIO_PowerHold );
 	require_noerr( platformStatus, exit );
+	
+	// Turn on the LED to show the system is on
+	status = FQuadTXLED_On();
+	require_noerr( status, exit );
 		
 	status = FStatus_Success;
 exit:
@@ -45,6 +73,56 @@ FStatus FQuadTXPower_Release()
 {
 	FStatus status = FStatus_Failed;
 	PlatformStatus platformStatus;
+		
+	// Release power hold, which will turn this MCU off when the user releases the on button
+	platformStatus = PlatformGPIO_OutputLow( FQuadTXGPIO_PowerHold );
+	require_noerr( platformStatus, exit );
+		
+	status = FStatus_Success;
+exit:
+	return status;
+}
+
+FStatus FQuadTXPower_CheckPowerOffRequest()
+{
+	FStatus status = FStatus_Failed;
+	PlatformStatus platformStatus;
+	uint64_t currentTime;
+	bool isButtonPressed;
+	
+// 	status = FQuadTXPad_ReadButtonState( FQuadTXPadButton_Center, &isButtonPressed );
+// 	require_noerr( status, exit );
+// 	
+// 	// If the button is released, then the next press should reset mLastPressedTime
+// 	if ( !isButtonPressed )
+// 	{
+// 		mShouldResetTimerOnNextPress = true;
+// 	}
+// 	else // Button is being held
+// 	{	
+// 		// Reset mLastPressedTime if the button was just pressed
+// 		if ( mShouldResetTimerOnNextPress )
+// 		{
+// 			status = PlatformTimer_GetTime( &mLastPressedTime );
+// 			require_noerr( status, exit );
+// 			mShouldResetTimerOnNextPress = false;
+// 		}
+// 		
+// 		// Get the current time, and check if the button has been held for at least FQUADTX_POWER_RELEASE_REQUEST_WAIT_MS
+// 		status = PlatformTimer_GetTime( &currentTime );
+// 		require_noerr( status, exit );
+// 		
+// 		if (( currentTime - mLastPressedTime ) > FQUADTX_POWER_RELEASE_REQUEST_WAIT_MS )
+// 		{
+// 			// Release the power line
+// 			status = FQuadTXPower_Release();
+// 			require_noerr( status, exit );
+// 			
+// 			// Turn off the LED to indicate the system is shutting down
+// 			status = FQuadTXLED_Off();
+// 			require_noerr( status, exit );
+// 		}
+// 	}
 		
 	// Release power hold, which will turn this MCU off when the user releases the on button
 	platformStatus = PlatformGPIO_OutputLow( FQuadTXGPIO_PowerHold );
